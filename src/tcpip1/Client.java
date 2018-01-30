f24f269a30e7f4e3a896456a1068d44206fe9079
student <student@M110312>
2018-01-30 14:04:10 +0900

    20180130 1404

--- src/tcpip1/Client.java
+++ src/tcpip1/Client.java
@@ -1,7 +1,52 @@
 package tcpip1;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
-  public static void name(String args[]){
-  //호잇
-  }
+	public static void main(String args[]) {
+		String ip = "70.12.111.130";
+		int port = 7777;
+		Socket socket = null;
+		OutputStream outs = null;
+		OutputStreamWriter outw = null;
+		try {
+			System.out.println("Start Client ....");
+			socket = new Socket(ip, port);
+			System.out.println("Connected Ok ....");
+
+			outs = socket.getOutputStream();
+			outw = new OutputStreamWriter(outs);
+			outw.write(" 해킹 시작 "
+					+ " @ "
+					+ " 1 "
+					+ " @ "
+					+ " 2 "
+					+ " @ "
+					+ " 3 "
+					+ " @ "
+					+ " 4 "
+					+ "해킹 성공");
+			System.out.println("Send Completed");
+
+		} catch (UnknownHostException e) {
+			System.out.println("UnkownHostException ....");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IOException ....");
+			e.printStackTrace();
+		} finally {
+			try {
+				outs.close();
+				outw.close();
+				socket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- src/tcpip1/Server.java
+++ src/tcpip1/Server.java
@@ -1,5 +1,53 @@
 package tcpip1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+	public static void main(String[] args) {
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+		InputStream in = null;
+		InputStreamReader inr = null;
+		BufferedReader br = null;
+		try {
+			serverSocket = new ServerSocket(6666);
+			InetAddress ia = InetAddress.getLocalHost();
+			System.out.println(ia.getHostName());
+			System.out.println(ia.getHostAddress());
+			System.out.println("Server Start ....");
+			while (true) {
+				socket = serverSocket.accept();
+				System.out.println("Client Connected ....");
+
+				// Receive Data ....
+				in = socket.getInputStream();
+				inr = new InputStreamReader(in);
+				br = new BufferedReader(inr);
+				String receiveStr = br.readLine();
+				System.out.println(receiveStr);
+				System.out.println("End Server ....");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+				inr.close();
+				in.close();
+				socket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
 
 }
